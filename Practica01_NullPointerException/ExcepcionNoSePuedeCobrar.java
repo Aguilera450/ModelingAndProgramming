3755860044843c749f0e1d54164c58da067cca2c
Aguilera450 <aguilera@ciencias.unam.mx>
2022-03-11 00:24:32 -0600

    Se crea la clase CuentaBancaria.java, donde se simula el estado del dinero disponible en la cuenta y se realizan cobros. Se crea la clase ExcepcionNoSePuedeCobrar.java, que extiende a Exception y se implementa para lidiar las posibles excepciones en la clase CuentaBancaria. Se implmentan los respectivos método y atributos para cada clase [los que se encuentran en el UML].

---
+++ Practica01_NullPointerException/ExcepcionNoSePuedeCobrar.java
@@ -0,0 +1,24 @@
+/**
+ * Clase para excepciones de cobro, cuando se intenta
+ * cobrar una cnatidad mayor a la disponible se debe
+ * invocar a esta excepción.
+ * @author Adrian Aguilera, Marco Rivera, Diego Rosas.
+ * @date 10/03/2021
+ */
+
+public class ExcepcionNoSePuedeCobrar extends Exception {
+    
+    /**
+     * Constructor por omisión.
+     */
+    public ExcepcionNoSePuedeCobrar() {}
+    
+    /**
+     * Constructor por parámetro.
+     * @param <code>mensaje</code> -- recibe un mensaje a mostrar
+     *                                cuando se lance la excepción.
+     */
+    public ExcepcionNoSePuedeCobrar(String mensaje) {
+	super(mensaje);
+    }
+}
