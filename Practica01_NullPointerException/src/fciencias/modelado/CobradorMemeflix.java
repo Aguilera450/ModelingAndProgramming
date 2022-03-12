dbd11e76953c4a35c75f0557ac87ce50b3451b25
= <=>
2022-03-11 20:10:11 -0600

    Practica 01: Se realizó Strategy en el servicio Memeflix. Mediante CobroMemeflix, MemeflixUnDispositivo, MemeflixDosDispositivos y MemeflixCuatroDispositivos.

---
+++ Practica01_NullPointerException/src/fciencias/modelado/CobradorMemeflix.java
@@ -0,0 +1,19 @@
+package src.fciencias.modelado;
+
+/**
+ * Interfaz para el patrón Strategy en el servicio Memeflix.
+ * Distintos tipos de cobro para el servicio Memeflix podran implementar esta interfaz.
+ * @author Adrian Aguilera Moreno     - Aguiler450
+ * @author Rosas Franco Diego Angel   - shikitimiau
+ * @author Marco Antonio Rivera Silva - DONMARCORS
+ * @version  1.0 - 11/03/2022
+ */
+public interface CobradorMemeflix {
+
+   /**
+     * Realiza el cobro del servicio Memeflix a la suscripción recibida.
+     * @param suscripcion - suscripción a la que se le hará el cobro.
+     * @return True si el cliente tiene fondos para pagar la suscripción, False si no pudo pagarla.
+     */
+   public boolean realizar_cobro(Suscripcion suscripcion);
+}
---
+++ Practica01_NullPointerException/src/fciencias/modelado/MemeflixDosDispositivos.java
@@ -0,0 +1,30 @@
+package src.fciencias.modelado;
+
+/**
+ * Tipo de cobro para las suscripciones que contrataron el servicio de Memeflix para dos dispositivos.
+ * Esta clase forma parte del patrón Strategy en la clase Memeflix.
+ * @author Adrian Aguilera Moreno     - Aguiler450
+ * @author Rosas Franco Diego Angel   - shikitimiau
+ * @author Marco Antonio Rivera Silva - DONMARCORS
+ * @version  1.0 - 11/03/2022
+ */
+public class MemeflixDosDispositivos implements CobradorMemeflix{
+    /** Precio asignado al servicio Memeflix para dos dispositivos */
+    private final float precio = 170;
+
+    /**
+     * Realiza el cobro del servicio Memeflix para dos dispositivos a la suscripción recibida.
+     * @param suscripcion - suscripción a la que se le hará el cobro.
+     * @return True si el cliente tiene fondos para pagar la suscripción, False si no pudo pagarla.
+     */
+    @Override
+    public boolean realizar_cobro(Suscripcion suscripcion){
+        if(suscripcion.metodo_pago().saldo_disponible() >= precio){
+            suscripcion.metodo_pago().cobrar(precio, "Servicio Memeflix para dos dispositivos");
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+}
