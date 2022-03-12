55803dab51f1988f0a7c664b8bb06d1db20a5e60
= <=>
2022-03-11 20:32:13 -0600

    Practica 01: Se realizó Strategy en el servicio Spootify. Mediante CobradorSpootify, SpootifyNormal y SpootifyPremium.

---
+++ Practica01_NullPointerException/src/fciencias/modelado/CobradorSpootify.java
@@ -0,0 +1,19 @@
+package src.fciencias.modelado;
+
+/**
+ * Interfaz para el patrón Strategy en el servicio Spootify.
+ * Distintos tipos de cobro para el servicio Spootify podran implementar esta interfaz.
+ * @author Adrian Aguilera Moreno     - Aguiler450
+ * @author Rosas Franco Diego Angel   - shikitimiau
+ * @author Marco Antonio Rivera Silva - DONMARCORS
+ * @version  1.0 - 11/03/2022
+ */
+public interface CobradorSpootify {
+
+   /**
+     * Realiza el cobro del servicio Spootify a la suscripción recibida.
+     * @param suscripcion - suscripción a la que se le hará el cobro.
+     * @return True si el cliente tiene fondos para pagar la suscripción, False si no pudo pagarla.
+     */
+   public boolean realizar_cobro(Suscripcion suscripcion);
+}
---
+++ Practica01_NullPointerException/src/fciencias/modelado/SpootifyPremium.java
@@ -0,0 +1,30 @@
+package src.fciencias.modelado;
+
+/**
+ * Tipo de cobro para las suscripciones que contrataron el servicio de Spootify Premium.
+ * Esta clase forma parte del patrón Strategy en la clase Spootify.
+ * @author Adrian Aguilera Moreno     - Aguiler450
+ * @author Rosas Franco Diego Angel   - shikitimiau
+ * @author Marco Antonio Rivera Silva - DONMARCORS
+ * @version  1.0 - 11/03/2022
+ */
+public class SpootifyPremium implements CobradorSpootify{
+    /** Precio asignado al servicio Spootify Premium */
+    private final float precio = 80;
+
+    /**
+     * Realiza el cobro del servicio Spootify Premium a la suscripción recibida.
+     * @param suscripcion - suscripción a la que se le hará el cobro.
+     * @return True si el cliente tiene fondos para pagar la suscripción, False si no pudo pagarla.
+     */
+    @Override
+    public boolean realizar_cobro(Suscripcion suscripcion){
+        if(suscripcion.metodo_pago().saldo_disponible() >= precio){
+            suscripcion.metodo_pago().cobrar(precio, "Servicio Spootify Premium");
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+}
