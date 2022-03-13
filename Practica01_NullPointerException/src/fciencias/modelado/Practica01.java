7dc479f478bde54d6c78c963b2e757db183bde6c
DONMARCORS <marcoantonioriverasilva@ciencias.unam.mx>
2022-03-12 19:32:20 -0600

    Agregada clase princial llamada Practica01

---
+++ Practica01_NullPointerException/src/fciencias/modelado/Practica01.java
@@ -0,0 +1,15 @@
+package src.fciencias.modelado;
+
+public class Practica01{
+    
+    public static void main(String[] args) {
+        Memeflix memeflix = new Memeflix("Hola","Bienvenido","Bienvenido de vuelta");
+        //Thisney thisney = new Thisney("Hola","Bienvenido","Bienvenido de vuelta");
+        //Momazon momazon = new Momazon("Hola","Bienvenido","Bienvenido de vuelta");
+        //Spootify spootify = new Spootify("Hola","Bienvenido","Bienvenido de vuelta");
+        //HVOMax hvomax = new HVOMax("Hola","Bienvenido","Bienvenido de vuelta");
+        for(int i = 0; i<12; i++){
+
+        }   
+    }
+}
