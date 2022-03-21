24b45286c162a17f60c7f9b4a293b8171d74d5f6
Aguilera450 <aguilera@ciencias.unam.mx>
2022-03-21 11:08:00 -0600

    UPDATE: se anexan los esquelos a las clases con prefijo Mc.

---
+++ Practica02_NullPointerException/src/fciencias/modelado/McNopalitoToreado.java
@@ -0,0 +1,89 @@
+/**
+ *
+ */
+
+public McNopalitoToreado {
+    
+    /**
+     *
+     */
+    public McNopalitoToreado() {
+	
+    }
+    
+    /**
+     * 
+     */
+    @Override
+    public String cocinar() {
+	return "No implementado.";
+    }
+    
+    /**
+     * Método que devuelve el estado del pan en la
+     * base de la hamburguesa.
+     * @return <code>String</code> -- mensaje que le
+     * dice al usuario como va el avance del pan en
+     * la base de la hamburguesa.
+     */
+    @Override
+    public String ponerBasePan() {
+	return " ";
+    }
+    
+    /**
+     *
+     */
+    @Override
+    public String ponerMayonesa() {
+	return " ";
+    }
+    
+    /**
+     *
+     */
+    @Override
+    public String ponerMostaza() {
+	return " ";
+    }
+    
+    /**
+     *
+     */
+    @Override
+    public String prepararCarne() {
+	return " ";
+    }
+    
+    /**
+     * 
+     */
+    @Override
+    public String ponerQueso() {
+	return " ";
+    }
+    
+    /**
+     *
+     */
+    @Override
+    public String ponerVegetales() {
+	return " ";
+    }    
+    
+    /**
+     *
+     */
+    @Override
+    public String ponerCatsup() {
+	return " ";
+    }
+    
+    /**
+     *
+     */
+    @Override
+    public String tapaPan() {
+	return " ";
+    }
+}
