09f1cdc13766dbc310314878ea783386433a2d17
Aguilera450 <aguilera@ciencias.unam.mx>
2022-03-22 17:21:16 -0600

    UPDATE: se anexa la documentación de cabecera a las clases.

--- Practica02_NullPointerException/src/fciencias/modelado/ModoComanda.java
+++ Practica02_NullPointerException/src/fciencias/modelado/ModoComanda.java
@@ -1,3 +1,12 @@
+/**
+ * Clase que simula el estado <code>ModoComanda</code> del Robot.
+ * @author Adrian Aguilera Moreno - Aguiler450
+ * @author Rosas Franco Diego Angel - shikitimiau
+ * @author Marco Antonio Rivera Silva - DONMARCORS
+ * @version 1.0 - 22/03/2022
+ * @see <code>EstadoRobot</code>.
+ */
+
 public class ModoComanda implements EstadoRobot{
     /** Robot asociado al estado actual */
     private Robot robot;
