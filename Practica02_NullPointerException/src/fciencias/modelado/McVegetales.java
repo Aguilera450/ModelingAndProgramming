6977dcd744df9428568478286603409dfb047aeb
Aguilera450 <aguilera@ciencias.unam.mx>
2022-03-22 03:48:27 -0600

    UPDATE: modificaciones en McVegetales.

--- Practica02_NullPointerException/src/fciencias/modelado/McVegetales.java
+++ Practica02_NullPointerException/src/fciencias/modelado/McVegetales.java
@@ -1,22 +1,46 @@
 /**
- *
+ * Clase que simula al objeto hamburgesa <code>McVegetales</code>.
+ * @author Adrian Aguilera Moreno     - Aguiler450
+ * @author Rosas Franco Diego Angel   - shikitimiau
+ * @author Marco Antonio Rivera Silva - DONMARCORS
+ * @version 1.0 - 22/03/2022
+ * @see <code>Platillo</code>.
  */
 
 public class McVegetales {
     
     /**
-     *
+     * Constructor por omisión de una hamburguesa de
+     * McVegetales.
      */
     public McVegetales() {
-	
+	super.id = 11;
+	super.nombre = "Hamburguesa McVegetales.";
+	super.descripcion = "McVegetales hamburguesa vegetariana."
+	super.precio = 39.50f;
+	super.llevaQueso = true;
+	super.esVegetariano = true;
     }
     
     /**
-     * 
+     * Método que devuelve la preparación de la hamburguesa.
+     * @return <code>String</code> -- los pasos de cocinar una 
+     *                                <code>McPepinillos</code>. 
      */
     @Override
     public String cocinar() {
-	return "No implementado.";
+	return "********** "
+	    + "La preparacion de su hamburguesa esta hecha en los siguientes pasos:"
+	    + "\n 1. " + ponerBasePan()
+	    + "\n 2. " + ponerMayonesa()
+	    + "\n 3. " + ponerMostaza()
+	    + "\n 4. " + prepararCarne()
+	    + "\n 5. " + ponerCarne()
+	    + "\n 6. " + ponerQueso()
+	    + "\n 7. " + ponerVegetales()
+	    + "\n 8. " + ponerCatsup()
+	    + "\n 9. " + ponerTapaPan()
+	    + "\n ¡Gracias por la espera!";
     }
     
     /**
