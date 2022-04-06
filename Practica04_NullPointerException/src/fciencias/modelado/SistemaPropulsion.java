19b163e640f4953c21567605c166a1b1ceb047f2
Aguilera450 <aguilera@ciencias.unam.mx>
2022-04-06 00:18:46 -0500

    UPDATE: se anexa implementación a las clases <SistemaPropulsion, ViajeIntercontinental>.

---
+++ Practica04_NullPointerException/src/fciencias/modelado/SistemaPropulsion.java
@@ -0,0 +1,26 @@
+/**
+ * Clase abstracta que modela un <code>Sistema de Propulsión</code>.
+ * @author Adrian Aguilera Moreno     - Aguiler450
+ * @author Rosas Franco Diego Angel   - shikitimiau
+ * @author Marco Antonio Rivera Silva - DONMARCORS
+ * @version 1.0 - 05/04/2022
+ * @see <code>Componentes</code>.
+ */
+
+public class abstract SistemaPropulsion extends Componentes {
+    
+    /**
+     * Constructor por omisión que crea e inicializa un Sistema 
+     * de Propulsión para una nave espacial.
+     */
+    public SistemaPropulsion() {
+	super.nombre = "Sistema de propulsion tipo: ";
+	super.descripcion = "Sistema de propulsion: ";
+	super.precio = 0.0f;
+	super.velocidad = 0;
+	super.defensa = 0;
+	super.peso = 0.0f;
+	super.ataque = 0;
+    }
+    
+}
---
+++ Practica04_NullPointerException/src/fciencias/modelado/ViajeIntercontinental.java
@@ -0,0 +1,31 @@
+/**
+ * Clase que simula un <code>ViajeIntercontinental</code>.
+ * @author Adrian Aguilera Moreno     - Aguiler450
+ * @author Rosas Franco Diego Angel   - shikitimiau
+ * @author Marco Antonio Rivera Silva - DONMARCORS
+ * @version 1.0 - 05/04/2022
+ * @see <code>SistemaPropulsion</code>.
+ * @see <code>Componentes</code>.
+ */
+
+public class abstract ViajeIntercontinental extends SistemaPropulsion {
+    
+    /**
+     * Constructor por omisión de un <code>ViajeIntercontinental</code>.
+     * <p> Se concatenan o autosuman los valores referentes a este tipo
+     * de viaje en específico al constructor general hecho en la clase
+     * <code>SistemaPropulsion</code>.</p>
+     */
+    public class ViajeIntercontinental() {
+	super();
+	super.nombre += "Viaje Intercontinental.";
+	super.descripcion += "Este sistema es utilizado para viajar "
+	    + "entre continentes y dentro del planeta receptor (en "
+	    + "el que se encuentre la nave espacial).";
+	super.precio += 1980.50f;
+	super.peso += 4000.00f;
+	super.velocidad += 1;
+	super.defensa += 1;
+	super.ataque += 1;
+    }
+}
