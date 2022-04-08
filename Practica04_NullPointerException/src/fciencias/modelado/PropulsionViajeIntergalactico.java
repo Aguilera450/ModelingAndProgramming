72cf5274dcd29cc3697f27525ff8f2b854cd3b91
shikitimiau <=>
2022-04-08 16:17:53 -0500

    Practica 04: Correccion de componentes de propulsion.

---
+++ Practica04_NullPointerException/src/fciencias/modelado/PropulsionViajeIntergalactico.java
@@ -0,0 +1,30 @@
+/**
+ * Clase que simula un <code>SistemaPropulsion</code> de tipo <code>PropulsionViajeIntergalactico</code>.
+ * @author Adrian Aguilera Moreno     - Aguiler450
+ * @author Rosas Franco Diego Angel   - shikitimiau
+ * @author Marco Antonio Rivera Silva - DONMARCORS
+ * @version 1.0 - 08/04/2022
+ * @see <code>SistemaPropulsion</code>.
+ * @see <code>Componente</code>.
+ */
+
+public abstract class PropulsionViajeIntergalactico extends SistemaPropulsion {
+
+    /**
+     * Constructor de un <code>PropulsionViajeIntergalactico</code>.
+     * Se concatenan o autosuman los valores referentes a este tipo de viaje.
+     */
+    public PropulsionViajeIntergalactico() {
+		// Se inicializan los valores en general
+		super();
+		// Se modifican los valores relacionados al componente.
+		super.nombre += "Viaje Intergalactico.";
+		super.descripcion += "Este sistema es utilizado para viajar "
+			+ "entre galaxias y dentro de algún k-universo (para alguna "
+			+ "0 < k < ∞, y en el que se encuentre la nave espacial).";
+		super.precio += 898000.50f;
+		super.peso += 24000.49f;
+		super.velocidad += 9;
+		super.defensa += 3;
+    }    
+}
