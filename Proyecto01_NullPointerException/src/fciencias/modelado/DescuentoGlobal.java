0b4b05d2c8f98226244b657c9a10814be070b956
Aguilera450 <aguilera@ciencias.unam.mx>
2022-05-06 16:23:51 -0500

    UPDATE: corrección de constructores de Descuento<> y anexión de documentación.

--- Proyecto01_NullPointerException/src/fciencias/modelado/DescuentoGlobal.java
+++ Proyecto01_NullPointerException/src/fciencias/modelado/DescuentoGlobal.java
@@ -1,7 +1,13 @@
 /**
- *
+ * Clase <code>DescuentoGlobal</code> que aplica el patrón decorator
+ * para realizar los descuentos correspondientes a un <code>Producto</code> 
+ * que contemple dicho descuento.
+ * @author Adrian Aguilera Moreno     - Aguiler450
+ * @author Rosas Franco Diego Angel   - shikitimiau
+ * @author Marco Antonio Rivera Silva - DONMARCORS
+ * @see <code>ProductoConDescuento</code>.
+ * @version 1.0 - 06/05/2022
  */
-
 public class DescuentoGlobal {
     
         
--- Proyecto01_NullPointerException/src/fciencias/modelado/DescuentoMexico.java
+++ Proyecto01_NullPointerException/src/fciencias/modelado/DescuentoMexico.java
@@ -1,18 +1,32 @@
 /**
- *
+ * Clase <code>DescuentoMexico</code> que aplica el patrón decorator
+ * para realizar los descuentos correspondientes a un <code>Producto</code> 
+ * que contemple dicho descuento.
+ * @author Adrian Aguilera Moreno     - Aguiler450
+ * @author Rosas Franco Diego Angel   - shikitimiau
+ * @author Marco Antonio Rivera Silva - DONMARCORS
+ * @see <code>ProductoConDescuento</code>.
+ * @version 1.0 - 06/05/2022
  */
 
 public class DescuentoMexico extends Descuento {
     
     /**
-     *
+     * Constructor por parámetros de un <code>DescuentoEspania</code>.
+     * @param <code>producto</code> -- al cual se le aplica el descuento.
      */
-    public Descuento(Producto producto) {
+    public DescuentoMexico(Producto producto) {
 	super(producto);
     }
     
     /**
-     *
+     * Método que calcula el precio de nuestro producto después de aplcicar
+     * el descuento correspondiente (que es elejido al azar).
+     * @param <code>usuario</code> -- usuario que adquiere el producto,
+     *                                este usuario sirve para cerciorarse
+     *                                que pueda aplicar al descuento.
+     * @return <code>float</code>  -- nuevo precio del producto, después de
+     *                                aplicarse el descuento.
      */
     public float getPrecio(Usuario usuario) {
 	if(usuario.getPais().equalsIgnoreCase("Mexico") && producto.getPrecio())
@@ -23,7 +37,12 @@ public class DescuentoMexico extends Descuento {
     }
     
     /**
-     *
+     * Método que nos regresa la información del <code>Producto</code> que adquiere
+     * el <code>Usuario</code> con la anexión de su respectivo descuento.
+     * @param <code>usuario</code> -- nos cercioramos que el descuento aplique para
+     *                                este usuario en partícular.
+     * @return <code>String</code> -- información del producto con la anexión de su
+     *                                respectivo descuento.
      */
     public String getCatalogInf(Usuario usuario) {
 	if(usuario.getPais().equalsIgnoreCase("Mexico") && producto.getPrecio())
