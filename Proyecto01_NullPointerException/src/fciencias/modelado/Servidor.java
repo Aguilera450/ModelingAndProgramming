aaf6e8638c36803fd577192995c4172adf66fa22
shikitimiau <=>
2022-05-06 20:39:26 -0500

    Proyecto 01: Servidor.

--- Proyecto01_NullPointerException/src/fciencias/modelado/Servidor.java
+++ Proyecto01_NullPointerException/src/fciencias/modelado/Servidor.java
@@ -191,22 +191,24 @@ public class Servidor implements InterfazServidor{
     }
 
     public static void main(String[] args) {
-      /*
       Usuario ross = new Usuario("Ross", "Rosa Victoria Villa Padilla", "Zeldaqwerty",4510467245L, "Calle Olivos 145, Azulejos, Madrid", 0124317234L, "Espania");
-
+      
       Usuario arturGod = new Usuario("arturGod", "Arturo Lemus Pablo", "Password",5512235681L, "5ta Avenida No 45, Chicago", 1234543210L, "Estados Unidos");
       
       Usuario fulano = new Usuario("fulano", "Fulano Federico Fernandez Tragedio", "diosEstaAqui123",5578551018L, "Calle Chongus 132, Colonia De las Tragedias", 0224716230L, "Mexico");
-
+      
       listaUsuarios = new HashMap<>();
       listaUsuarios.put("Ross",ross);
       listaUsuarios.put("arturGod",arturGod);
       listaUsuarios.put("fulano",fulano);
       
       guardarUsuarios(listaUsuarios);
-      */
+
+      Producto a1 = new Producto("La Costeña Chile Jalapeño Rajas 105 Gr");
+      /*
       recuperarArticulos();
       System.out.println(listaUsuarios.get("fulano").getNombreCompleto());
+      */
 
 
       
